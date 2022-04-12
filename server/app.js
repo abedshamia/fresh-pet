e63fa3398dec91fffabab1965331ad513f571d7b
abedshamia <ab.shamia96@gmail.com>
2022-04-13 02:22:44 +0300

    add hide powered by

--- server/app.js
+++ server/app.js
@@ -1,17 +1,18 @@
 // const { join } = require('path');
 const express = require('express');
-const helmet = require('helmet');
+const {hidePoweredBy} = require('helmet');
+//use it
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const router = require('./routes');
 
-
 const app = express();
 app.use(cookieParser());
 app.disable('x-powered-by');
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(express.json());
-app.use(helmet());
+app.use(hidePoweredBy());
+
 app.use(cors());
 
 app.use(router);
